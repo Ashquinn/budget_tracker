e35d23cd8326119d95b0ef69aef9918466d52571
Ashquinn Gordon <ashquinngordon@gmail.com>
2021-07-15 23:57:18 -0400

    service-worker file updated, paths to bundle.js and manifest.json have been added

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+const FILES_TO_CACHE = [
+    "/",
+    "index.html",
+    "assets/css/style.css",
+    "dist/bundle.js",
+    "dist/manifest.json",
+    "dist/icon-192x192.png",
+    "dist/icon-512x512.png"
+  ];
+  
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
