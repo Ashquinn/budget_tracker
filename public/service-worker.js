67623baf36d07514fefcab480f32daa35f588ffc
Ashquinn Gordon <ashquinngordon@gmail.com>
2021-07-21 19:37:05 -0400

    service-worker file has been updated with functionality to cache file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,73 @@
 const FILES_TO_CACHE = [
     "/",
-    "index.html",
-    "assets/css/style.css",
-    "dist/bundle.js",
-    "dist/manifest.json",
-    "dist/icon-192x192.png",
-    "dist/icon-512x512.png"
+    // "index.html",
+    "/assets/css/styles.css",
+    "/dist/bundle.js",
+    "/dist/manifest.json",
+    "/dist/icon_192x192.png",
+    "/dist/icon_512x512.png",
+    "/indexedDB.js"
   ];
   
   const CACHE_NAME = "static-cache-v2";
   const DATA_CACHE_NAME = "data-cache-v1";
+
+  // Boiler plate code, edits needed to enable functionality
+  // install
+self.addEventListener("install", function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", function(evt) {
+  if (evt.request.url.includes("/api/transaction")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  
+  evt.respondWith(
+    caches.match(evt.request).then(function(response) {
+      return response || fetch(evt.request);
+    })
+  );
+});
